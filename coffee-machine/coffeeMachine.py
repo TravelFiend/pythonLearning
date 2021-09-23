e220bf23e13eecfb5243eaa1db723956beec5746
TravelFiend <mikegrace274@gmail.com>
2021-09-22 19:54:54 -0700

    finished the udemy coffee machine

--- coffee-machine/coffeeMachine.py
+++ coffee-machine/coffeeMachine.py
@@ -31,39 +31,51 @@ resources = {
 }
 
 money = 0.0
+go = True
 
-customerChoice = input(' What would you like? (espresso/latte/cappuccino): ')
-print('Please insert coins.')
+while go:
+  customerChoice = input(' What would you like? (espresso/latte/cappuccino): ')
+  print('Please insert coins.')
 
-enoughWater = MENU[customerChoice]['ingredients']['water'] <= resources['water']
-enoughMilk = MENU[customerChoice]['ingredients']['milk'] <= resources['milk']
-enoughCoffee = MENU[customerChoice]['ingredients']['coffee'] <= resources['coffee']
-enoughResources = enoughWater and enoughMilk and enoughCoffee
-
-if customerChoice == 'report':
-  print(f'Water: {resources["water"]}\nMilk: {resources["milk"]}\nCoffee: {resources["coffee"]}\nMoney: ${money}')
-else:
-  quarters = float(input('How many quarters?: ')) * 25 / 100
-  dimes = float(input('How many dimes?: ')) * 10 / 100
-  nickels = float(input('How many nickels?: ')) * 5 / 100
-  pennies = float(input('How many pennies?: ')) / 100
-
-  totalPaid = quarters + dimes + nickels + pennies;
-
-  if totalPaid >= MENU[customerChoice]['cost'] and enoughResources:
-    money += MENU[customerChoice]["cost"]
-    resources['water'] -= MENU[customerChoice]['ingredients']['water']
-    resources['milk'] -= MENU[customerChoice]['ingredients']['milk']
-    resources['coffee'] -= MENU[customerChoice]['ingredients']['coffee']
-    print(f'Here is ${round(totalPaid - MENU[customerChoice]["cost"], 2)} in change.')
-    print(f'Here is your {customerChoice} ☕️ Enjoy!')
+  if customerChoice == 'report':
+    print(f'Water: {resources["water"]}\nMilk: {resources["milk"]}\nCoffee: {resources["coffee"]}\nMoney: ${money}')
   else:
-    outOf = ''
-    if not enoughWater:
-      outOf = 'water'
-    elif not enoughMilk:
-      outOf = 'milk'
-    elif not enoughCoffee:
-      outOf = 'coffee'
+    enoughWater = MENU[customerChoice]['ingredients']['water'] <= resources['water']
+    enoughMilk = MENU[customerChoice]['ingredients']['milk'] <= resources['milk']
+    enoughCoffee = MENU[customerChoice]['ingredients']['coffee'] <= resources['coffee']
+    enoughResources = enoughWater and enoughMilk and enoughCoffee
 
-    print(f'Sorry, there is not enough {outOf}.')
+    if not enoughResources:
+      outOf = ''
+      if not enoughWater:
+        outOf = 'water'
+      elif not enoughMilk:
+        outOf = 'milk'
+      elif not enoughCoffee:
+        outOf = 'coffee'
+      print(f'Sorry, there is not enough {outOf}.')
+    else:
+      quarters = float(input('How many quarters?: ')) * 25 / 100
+      dimes = float(input('How many dimes?: ')) * 10 / 100
+      nickels = float(input('How many nickels?: ')) * 5 / 100
+      pennies = float(input('How many pennies?: ')) / 100
+
+      totalPaid = quarters + dimes + nickels + pennies;
+
+      if totalPaid >= MENU[customerChoice]['cost'] and enoughResources:
+        money += MENU[customerChoice]["cost"]
+        resources['water'] -= MENU[customerChoice]['ingredients']['water']
+        resources['milk'] -= MENU[customerChoice]['ingredients']['milk']
+        resources['coffee'] -= MENU[customerChoice]['ingredients']['coffee']
+        print(f'Here is ${round(totalPaid - MENU[customerChoice]["cost"], 2)} in change.')
+        print(f'Here is your {customerChoice} ☕️ Enjoy!')
+      else:
+        outOf = ''
+        if not enoughWater:
+          outOf = 'water'
+        elif not enoughMilk:
+          outOf = 'milk'
+        elif not enoughCoffee:
+          outOf = 'coffee'
+        print(f'Sorry, there is not enough {outOf}.')
+
