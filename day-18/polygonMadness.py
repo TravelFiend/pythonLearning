0d531e6dddebcebe5797932412f788eda5b29e5d
TravelFiend <mikegrace274@gmail.com>
2021-10-31 22:51:45 -0700

    lines are now colorful

--- day-18/polygonMadness.py
+++ day-18/polygonMadness.py
@@ -2,17 +2,18 @@ from turtle import Turtle, Screen
 import turtle
 
 tabby = Turtle()
+tabby.shape('turtle')
 
 colors = ['purple', 'slate blue', 'royal blue', 'cadet blue', 'turquoise', 'cyan', 'aquamarine', 'spring green', 'lime green', 'forest green']
 angleTotal = 180
 
 for n in range(3, 11):
-  angle = 180 - (angleTotal / n)
+  tabby.color(colors[n - 3])
 
   for p in range(0, n):
-    tabby.color(colors[n - 3])
-    turtle.forward(100)
-    turtle.right(angle)
+    angle = 180 - (angleTotal / n)
+    tabby.forward(100)
+    tabby.right(angle)
 
   angleTotal += 180
 
