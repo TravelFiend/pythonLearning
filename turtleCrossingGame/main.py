cd4a66862519edc73042b8e509b83c917ea31b53
TravelFiend <mikegrace274@gmail.com>
2021-11-17 00:29:31 -0800

    turtle crossing game done

--- turtleCrossingGame/main.py
+++ turtleCrossingGame/main.py
@@ -4,6 +4,7 @@ from classes.player import Player
 from classes.scoreboard import Scoreboard
 from classes.carManager import CarManager
 
+
 screen = Screen()
 screen.setup(600, 600)
 screen.bgcolor('black')
@@ -12,15 +13,27 @@ screen.tracer(0)
 
 turd = Player()
 scoreboard = Scoreboard()
+car = CarManager()
 
 screen.listen()
 screen.onkey(turd.move, 'Up')
 
+sleepTime = 0.1
 gameIsOn = True
 while gameIsOn:
-  time.sleep(0.1)
   screen.update()
+  time.sleep(sleepTime)
+  car.makeCar()
+  car.moveLeft()
+
+  for auto in car.allCars:
+    if auto.distance(turd) < 20:
+      gameIsOn = False
+      scoreboard.gameOver()
 
   if turd.ycor() > 280:
     turd.backToStart()
     scoreboard.updateLevel()
+    sleepTime *= 0.8
+
+screen.exitonclick()
